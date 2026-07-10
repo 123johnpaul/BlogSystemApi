bbb9e898567f0a123bed974da3ade1200ab3cdaa
123johnpaul <jhae440@gmail.com>
2026-07-10 12:13:19 +0100

    Initial Clean Architecture solution setup

---
+++ tests/Blog.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Blog.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
